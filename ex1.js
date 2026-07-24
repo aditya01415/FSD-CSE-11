a20c2e107de880021db14f46d790a1da533a68d2
Aditya101415 <official.aditya8787@gmail.com>
2026-07-24 10:09:04 +0530

    ..

---
+++ ex1.js
@@ -0,0 +1,11 @@
+var a = 10;
+var a = 30;
+console.log("a=",a);
+
+// let a = 10;
+// let a = 30;     //It will give error because we cannot redeclare a variable using let in the same scope.
+// console.log("a=",a);
+
+const c = 20;
+c = b;
+console.log("c=",c)
