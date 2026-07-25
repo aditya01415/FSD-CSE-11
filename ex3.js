05defc0259c9b8824ac261c73b4b829537437d1f
Aditya101415 <official.aditya8787@gmail.com>
2026-07-25 22:57:34 +0530

    ...

---
+++ ex3.js
@@ -0,0 +1,8 @@
+const a=[1,2,3,4,5,6,7,8,9];
+const even = a.filter((i)=>(i%2==0)) ;
+console.log("a=",a);
+console.log("Even=",even)
+const square = even.map((i)=>(i*i));
+console.log("Square=",square)
+const sum = square.reduce((i,s)=>(i+s))
+console.log("sum",sum)
