37bfdf73fc17be2c9cb12d78626be0ec9b73a343
Aditya101415 <official.aditya8787@gmail.com>
2026-09-01 15:07:48 +0530

    BOOK_NPM_APP

---
+++ BOOK_NPM_APP/Book.jsx
@@ -0,0 +1,16 @@
+
+function Book(){
+    return (
+        <>
+        <div>
+            {/* <img src="" width={100} height={130}  /> */}
+            <h2>Tittle: React</h2>
+            <h2>Price: ₹449-/</h2>
+            <button style={{color:"green"}}>Add To Cart</button>
+        </div>
+        
+        </>
+    )
+}
+
+export default Book;
---
+++ BOOK_NPM_APP/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+
+
+
+const parent = document.getElementById("root");
+const root = ReactDOM.createRoot(parent);
+root.render(<App/>);
